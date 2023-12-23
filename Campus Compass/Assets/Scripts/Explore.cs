9410a5d0e92d6a2666d9ecc4382f386679e30889
mihara16 <150780400+mihara16@users.noreply.github.com>
2023-12-24 00:24:10 +0530

    explore button
    added a new script where it loads to another scene when the explore button is clicked

---
+++ Campus Compass/Assets/Scripts/Explore.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Explore : MonoBehaviour
+{
+    public void LoadExploreSceneOnClick()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+        
+    }
+}
