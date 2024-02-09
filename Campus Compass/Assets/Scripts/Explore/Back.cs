2d6155b828f8afde1e0401de74c48cd32661aa4b
ameenk1 <ameen20016569@gmail.com>
2024-02-09 12:28:07 +0530

    settings

---
+++ Campus Compass/Assets/Scripts/Explore/Back.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadEditName()
+    {
+        SceneManager.LoadSceneAsync("EditName");
+
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadSceneAsync("Settings");
+
+    }
+
+    public void MainAppScene()
+    {
+        SceneManager.LoadSceneAsync("Main");
+
+    }
+
+
+}
